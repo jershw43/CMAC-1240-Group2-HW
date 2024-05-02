927b38ad598ba016a827583e4f3eea39dbd6a5f7
IsaacDerg <79806808+IsaacDerg@users.noreply.github.com>
2024-05-01 22:47:48 -0400

    Update gameFunctions.hpp

--- TextGame Project/gameFunctions.hpp
+++ TextGame Project/gameFunctions.hpp
@@ -1,11 +1,12 @@
 #include "playerFunctions.hpp"
+#include "inventoryFunctions.hpp"
 
 struct Enemy {
-    std::string name;
-    int health;
-    int attack;
+	std::string name;
+	int health;
+	int attack;
 };
 
 Enemy createRandomEnemy();
 void randomEvents(Player& player);
-void mainBattle(Player& player, Enemy& enemy);
+void mainBattle(Player& player,string inventory[],int& inventorySize, Enemy& enemy);
