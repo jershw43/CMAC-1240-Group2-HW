5f4c9212e8f1fd98c696d96b3e065ab5d4241e8a
IsaacDerg <79806808+IsaacDerg@users.noreply.github.com>
2024-05-01 22:47:30 -0400

    Update inventoryFunctions.cpp

--- TextGame Project/inventoryFunctions.cpp
+++ TextGame Project/inventoryFunctions.cpp
@@ -1,1 +1,23 @@
 #include "inventoryFunctions.hpp"
+#include <iostream>
+
+void addItemToInventory(string item, string inventory[], int& inventorySize){
+	if (inventorySize < MAX_ITEMS){
+		inventory[inventorySize++] = item;
+	}
+	else{
+		cout <<"Inventory is full" << endl;
+	}
+}
+
+void printInventory(string inventory[], int inventorySize){
+	if (inventorySize == 0){
+		cout << "Inventory is empty" << endl;
+	}
+	else {
+		for (int i = 0; i < inventorySize; i++){
+			cout << inventory[i] << endl;
+		}
+	}
+}
+			
