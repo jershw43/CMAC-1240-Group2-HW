99111faae43e452d1d03ddbdf36f67fce372361b
IsaacDerg <79806808+IsaacDerg@users.noreply.github.com>
2024-05-01 20:27:22 -0400

    Update gameFunctions.hpp

--- TextGame Project/gameFunctions.hpp
+++ TextGame Project/gameFunctions.hpp
@@ -1,4 +1,11 @@
 #include "playerFunctions.hpp"
 
+struct Enemy {
+    std::string name;
+    int health;
+    int attack;
+};
+
+Enemy createRandomEnemy();
 void randomEvents(Player& player);
-void mainBattle(Player& player);
+void mainBattle(Player& player, Enemy& enemy);
